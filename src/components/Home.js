4006fa9c26bb926375fc24bed2f5828e8020e209
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-08-06 10:40:18 +0500

    Designed the Home Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import profile from '../assets/images/profileImg.png';
+import bgImage from '../assets/images/blob-scatter-haikei.svg';
+
+const roles = ["HTML5 & CSS3", "JavaScript (ES6+)", "React.js", "Node.js", "Express.js", "MongoDB"];
 
 const Home = () => {
+    const [currentRole, setCurrentRole] = useState(0);
+    const [displayedText, setDisplayedText] = useState('');
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    useEffect(() => {
+        const handleText = () => {
+            const fullText = roles[currentRole];
+            const updatedText = isDeleting ? fullText.substring(0, displayedText.length - 1) : fullText.substring(0, displayedText.length + 1);
+            setDisplayedText(updatedText);
+
+            if (!isDeleting && updatedText === fullText) {
+                setTimeout(() => setIsDeleting(true), 1000);
+            } else if (isDeleting && updatedText === '') {
+                setIsDeleting(false);
+                setCurrentRole((prevRole) => (prevRole + 1) % roles.length);
+            }
+        };
+        const timer = setTimeout(handleText, isDeleting ? 50 : 150);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [displayedText, isDeleting, currentRole]);
+
     return (
-        <div className="bg-gray-900 text-white min-h-screen flex flex-col items-center justify-center text-center">
-            <h1 className="text-4xl md:text-6xl font-bold mb-4 animate-fade-in">
-                Hi, I’m Hamdan Raza
-            </h1>
-            <p className="text-lg md:text-2xl mb-6 animate-fade-in">
-                A passionate Developer
-            </p>
-            <div className="flex space-x-4 animate-fade-in">
-                <Link
-                    to="/projects"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                    View My Work
-                </Link>
-                <Link
-                    to="/contact"
-                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                >
-                    Contact Me
-                </Link>
+        <div style={{ backgroundImage: `url(${bgImage})` }} className="z-10 h-screen flex items-center justify-center text-white overflow-hidden">
+            <div className="container mx-auto px-6 flex flex-col md:flex-row items-center space-y-8 md:space-y-0 md:space-x-8 relative">
+                <div className="flex flex-col md:w-1/2 space-y-4 text-center md:text-left">
+                    <div className="inline-flex items-center animate-fade-in">
+                        <img src={profile} alt="Profile" className="w-16 h-16 mr-1 rounded-full" />
+                        <span className="ml-4 text-xl bg-white text-black p-4 rounded-full mx-auto md:mx-0">Hello friend!</span>
+                    </div>
+                    <h3 className="text-3xl md:text-3xl">I'm </h3>
+                    <h1 className="animate-slide-in text-5xl md:text-5xl font-extrabold">Hamdan Raza</h1>
+                    <div className="h-20 animate-slide-in">
+                        <h2 className="text-4xl md:text-4xl">{displayedText}</h2>
+                    </div>
+                    <div className="animate-fade-in flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mt-6">
+                        <button className="bg-white text-red-500 py-2 px-6 font-bold hover:bg-red-500 hover:text-white hover:border hover:border-black hover:animate-slide-in transition duration-300">Hire Me</button>
+                        <button className="bg-white text-red-500 py-2 px-6 font-bold hover:bg-red-500 hover:text-white hover:border hover:border-black hover:animate-slide-in transition duration-300">Contact Me</button>
+                    </div>
+                </div>
+                <div className="md:w-1/2 flex justify-center md:justify-end relative animate-fade-in">
+                    <img src={profile} alt="DP" className="h-96 md:w-full relative " />
+                </div>
             </div>
         </div>
     );
